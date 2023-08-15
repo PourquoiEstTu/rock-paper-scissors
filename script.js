411f28591aa7b1916d4b1586b518c355d154e90e
PourquoiEstTu <rsquyum@gmail.com>
2023-08-15 14:58:09 -0400

    Implement playRound()

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
 function getComputerChoice() {
     let randNum = Math.floor(Math.random() * 3);
-    console.log("The number chosen is: " + randNum);
+    // console.log("The number chosen is: " + randNum);
     switch (randNum) {
         case 0: 
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
         default:
             return "The Computer Could Not Choose!";
     }
 }
 
-console.log(getComputerChoice());
-
 function playRound(playerChoice, compChoice) {
-    
+    let lChoice = playerChoice.toLowerCase();
+    if (lChoice === "rock") {
+        switch (compChoice) {
+            case "rock":
+                return "Draw!";
+            case "paper":
+                return "You Lose! Paper beats Rock!";
+            case "scissors":
+                return "You Win! Rock beats Scissors";
+        }
+    } 
+    else if (lChoice === "paper") {
+        switch (compChoice) {
+            case "rock":
+                return "You Win! Paper beats Rock!";
+            case "paper":
+                return "Draw!";
+            case "scissors":
+                return "You Lose! Scissors beats Paper!";
+        }
+    }
+    else { //option for scissors
+        switch (compChoice) {
+            case "rock":
+                return "You Lose! Rock beats Scissors!";
+            case "paper":
+                return "You Win! Scissors beats Paper";
+            case "scissors":
+                return "Draw!";
+        }
+    }
 }
+
+// const playerSelection = "scissors";
+// const compSelection = getComputerChoice();
+// console.log(compSelection);
+// console.log(playRound(playerSelection, compSelection));
